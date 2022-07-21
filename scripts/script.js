bd95280faae01f8b446dc99014f999ee468f2f57
cpt arduous <mohitneg377@gmail.com>
2022-07-21 08:17:46 +0530

    Added playRound to the program it is now functional

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,46 @@ function getComputerChoice()
 }
 //alert(getComputerChoice());
 
+function playRound(playerSelection, computerSelection)
+{
+    if(playerSelection === "Rock")
+    {
+        if(computerSelection === "Scissor")
+        {
+            return("Hurray you win! Rocks beats scissors");
+            
+        }else if(computerSelection === "Paper")
+        {
+            return("You Lose! Paper beats rock");
+        }else
+        {
+            return("Deuce! Try again");
+        }
+    }else if(playerSelection === "Paper")
+    {
+        if(computerSelection === "Rock")
+        {
+            return("Hurray you win! Paper beats rock");   
+        }else if(computerSelection === "Scissor")
+        {
+            return("You Lose! Scissor beats paper");
+        }else
+        {
+            return("Deuce! Try again");
+        }
+    }else if(playerSelection === "Scissor")
+    {
+        if(computerSelection === "Rock")
+        {
+            return("You Lose! Rock beats scissor");   
+        }else if(computerSelection === "Paper")
+        {
+            return("Hurray you win! Scissor beats paper");
+        }else
+        {
+            return("Deuce! Try again");
+        }
+    }
+}
+
+console.log(playRound(getPlayerChoice(),getComputerChoice()));
